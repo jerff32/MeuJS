14beb2c89e0a440a4e8acbb95f33100200e8a72e
jerff32 <74563828+jerff32@users.noreply.github.com>
2021-07-09 09:40:43 -0300

    Update and rename paginacao.v1.js to paginacao.v2.js

--- paginacao.v2.js
+++ paginacao.v2.js
@@ -1,7 +1,3 @@
-	jQuery(function() {
-$("div.elementor-post-navigation__next > a").attr("id", "proximo-cap");
-$(".elementor-post-navigation__prev > a").attr('id', 'cap-anterior');
-});
 
 document.body.addEventListener("keydown", function(e) {
         if(39 === e.keyCode || 68 === e.keyCode) {
